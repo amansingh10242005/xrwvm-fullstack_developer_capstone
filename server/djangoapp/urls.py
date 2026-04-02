8a7d7b000e4a0ad2ae24c6492fc09f2eb31d258e
Aman Singh <amandeaman12345@gmail.com>
2026-04-02 19:55:10 +0530

    Module 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
     path(route='register', view=views.register_user, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ]
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .models import CarMake, CarModel
+from .populate import initiate
 
 
 # LOGIN VIEW
@@ -56,3 +58,14 @@ def register_user(request):
         )
         login(request, user)
         return JsonResponse({"userName": username, "status": "Authenticated"})
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
