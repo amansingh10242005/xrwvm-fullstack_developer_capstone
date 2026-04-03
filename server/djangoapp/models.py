6889bcd28d7d9b49bcde6bb85a108db9adce6db5
Aman Singh <amandeaman12345@gmail.com>
2026-04-03 13:10:52 +0530

    CI

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -12,9 +13,10 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    dealer_id = models.IntegerField(null=True) # Added dealer_id as it was mentioned "Dealer ID (IntegerField)" in the instructions
+    dealer_id = models.IntegerField(null=True)  # Added dealer_id
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -24,7 +26,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
+                               validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,7 +12,7 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+    if (kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
 
@@ -23,7 +23,7 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
         print("Network exception occurred")
 
@@ -43,5 +43,5 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as e:
         print("Network exception occurred")
