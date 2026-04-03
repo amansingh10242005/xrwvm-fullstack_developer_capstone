a3441bd595f67cf8e853eef209f1a6effadad722
Aman Singh <amandeaman12345@gmail.com>
2026-04-03 12:36:26 +0530

    MOdule 4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Route path="/" element={<h1>Home Page</h1>} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
